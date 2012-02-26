84b8139bc1b9a882c616e7abfa440bf9cc5bcfa6
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-02-26 01:18:19 +0100

    dalej niz waznego

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 #include <SDL/SDL.h>
 int main()
 {
-#ifdef __DEGUG
-	printf("Debug\n");
-#endif
-	SDL_Delay(100);
+	if ( SDL_Init(SDL_INIT_VIDEO) != 0 )
+	{
+		printf("Unable to initialize SDL: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 ); // *new*
+
+	SDL_Surface* screen = SDL_SetVideoMode( 640, 480, 16, SDL_OPENGL ); // *changed*
+	SDL_Delay(1000);
 	return 0;
 };
