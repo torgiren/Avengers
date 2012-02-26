c7b594aad69ef1a17043d3968748157f644a02e1
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-02-26 23:49:00 +0100

    GraphEngine: dodany viewport oraz zmiana zapisywania rozdzielczosci

--- src/graphengine.cpp
+++ src/graphengine.cpp
@@ -1,14 +1,17 @@
 #include "graphengine.h"
 GraphEngine::GraphEngine() throw(graph_init_error):
-	itsResolution(std::pair<int,int>(800,600)),
 	itsFullscreen(false),
 	itsScreen(false)
 {
+	itsResolution[0]=800;
+	itsResolution[1]=600;
 	if(SDL_Init(SDL_INIT_VIDEO)!= 0)
 	{
 	    throw graph_init_error();
 	}
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SetViewport(0,0,itsResolution[0],itsResolution[1]);
+	glEnable( GL_TEXTURE_2D );
 };
 GraphEngine* GraphEngine::GetInstance()
 {
@@ -22,7 +25,18 @@ bool GraphEngine::Reconfig()
 	Uint32 flags=SDL_OPENGL;
 	if(itsFullscreen)
 		flags|=SDL_FULLSCREEN;
-	itsScreen=SDL_SetVideoMode(itsResolution.first,itsResolution.second,16,flags);
+	itsScreen=SDL_SetVideoMode(itsResolution[0],itsResolution[1],16,flags);
+	glViewport( itsViewport[0],itsViewport[1],itsViewport[2],itsViewport[3]);
+	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+	glClear( GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT );
+	glEnable(GL_DEPTH_TEST);
+	//TODO Ulepszyć obsługę glOthod
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	glOrtho(0.0f, itsResolution[0], itsResolution[1], 0.0f, -1.0f, 1.0f);
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+	return true;
 };
 void GraphEngine::SetFullscreen(int full)
 {
@@ -31,11 +45,15 @@ void GraphEngine::SetFullscreen(int full)
 	else
 		itsFullscreen=full;
 };
-void GraphEngine::SetResolution(std::pair<int,int> res)
-{
-	itsResolution=res;
-};
 void GraphEngine::SetResolution(int width, int height)
 {
-	SetResolution(std::pair<int,int>(width,height));	
+	itsResolution[0]=width;
+	itsResolution[1]=height;
+};
+void GraphEngine::SetViewport(int x, int y, int width, int height)
+{
+	itsViewport[0]=x;
+	itsViewport[1]=y;
+	itsViewport[2]=width;
+	itsViewport[3]=height;	
 };
--- src/graphengine.h
+++ src/graphengine.h
@@ -1,23 +1,23 @@
 #ifndef __GRAPHENGINE_H
 #define __GRAPHENGINE_H
 #include <SDL/SDL.h>
+#include <GL/gl.h>
 #include <utility>
 #include "exceptions.h"
-/**
- * Klasa silnika graficznego
- */
 class GraphEngine
 {
 public:
 	static GraphEngine* GetInstance();
-	void SetResolution(int width, int heigth);
-	void SetResolution(std::pair<int,int> res);
-	void SetFullscreen(int full=-1);
 	bool Reconfig();
+
+	void SetResolution(int width, int height);
+	void SetFullscreen(int full=-1);
+	void SetViewport(int x,int y, int width, int height);
 private:
 	GraphEngine() throw(graph_init_error);
-	std::pair<int,int> itsResolution;
+	int itsResolution[2];
 	bool itsFullscreen;
+	int itsViewport[4];
 	SDL_Surface* itsScreen;
 };
 #endif
